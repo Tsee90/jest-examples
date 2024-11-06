2129925b80a6ea8deee00a2941abcc2f786bbeb0
Tsee90 <179171024+Tsee90@users.noreply.github.com>
2024-11-06 14:57:05 -0500

    feat: reverse string

--- example.test.js
+++ example.test.js
@@ -3,3 +3,7 @@ import * as main from './main.js';
 test('capitalize first letter', () => {
   expect(main.capitalize('hello')).toBe('Hello');
 });
+
+test('reverse string', () => {
+  expect(main.reverseString('hello')).toBe('olleh');
+});
