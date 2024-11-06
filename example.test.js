c217307420eefb04aa36641417f1e26365e8a6a4
Tsee90 <179171024+Tsee90@users.noreply.github.com>
2024-11-06 15:17:08 -0500

    feat: calculator

--- example.test.js
+++ example.test.js
@@ -7,3 +7,23 @@ test('capitalize first letter', () => {
 test('reverse string', () => {
   expect(main.reverseString('hello')).toBe('olleh');
 });
+
+test('calculator add', () => {
+  expect(main.calculator.add(3, 3)).toBe(6);
+});
+
+test('calculator subtract', () => {
+  expect(main.calculator.subtract(3, 6)).toBe(-3);
+});
+
+test('calculator multiply', () => {
+  expect(main.calculator.multiply(3, 3)).toBe(9);
+});
+
+test('calculator divide', () => {
+  expect(main.calculator.divide(9, 3)).toBe(3);
+});
+
+test('calculator divide by 0', () => {
+  expect(() => main.calculator.divide(3, 0)).toThrow('Cannot divide by 0');
+});
--- main.js
+++ main.js
@@ -5,3 +5,24 @@ export function capitalize(string) {
 export function reverseString(string) {
   return string.split('').reverse().join('');
 }
+
+export const calculator = {
+  add(a, b) {
+    return a + b;
+  },
+
+  subtract(a, b) {
+    return a - b;
+  },
+
+  multiply(a, b) {
+    return a * b;
+  },
+
+  divide(a, b) {
+    if (b === 0) {
+      throw new Error('Cannot divide by 0');
+    }
+    return a / b;
+  },
+};
